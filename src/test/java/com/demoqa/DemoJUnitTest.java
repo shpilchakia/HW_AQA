d74c7d23b056a39770790408a34356a0a09ddbc6
Igor Shpilchak <shpilchak.igor@yandex.ru>
2024-01-07 02:46:17 +0300

    add two .class, update build.gradle

---
+++ src/test/java/com/demoqa/DemoJUnitTest.java
@@ -0,0 +1,47 @@
+package com.demoqa;
+
+import org.junit.jupiter.api.*;
+
+import java.lang.module.Configuration;
+
+public class DemoJUnitTest {
+
+    Calculator calc;
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("   ### @BeforeAll");
+        //Configuration.browser = "chrome";
+    }
+
+    @AfterAll
+    static void afterAll(){
+        System.out.println("   ### @AfterAll");
+        //Configuration.browser = "chrome";
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("   ### @BeforeEach");
+        calc = new Calculator();
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println(" ### @afterEach");
+    }
+
+    @Test
+    void firstTest() {
+        System.out.println("   ### @Test firstTest");
+        int result = calc.sum(2,2);
+        Assertions.assertTrue(result == 4);
+    }
+
+    @Test
+    void secondTest() {
+        System.out.println("   ### @Test secondTest");
+        int result = calc.mult(2, 2);
+        Assertions.assertTrue(result == 4);
+    }
+}
